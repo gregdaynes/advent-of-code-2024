e6adc5eb23c5ab2bb275d4e5977d4d461f944675
Gregory Daynes <email@gregdaynes.com>
2024-12-02 09:37:34 -0800

    chore(02): pt2 dead end with this way
    This approach seems ok through the samples, and extended test cases, but
    the edge cases where the first number needs to be removed and rerun
    isn't possible.

--- day-02.js
+++ day-02.js
@@ -42,3 +42,115 @@ export function p1a (input) {
 
   return reports
 }
+
+export function p2a (input) {
+  const start = performance.now()
+  const reports = input.split('\n')
+
+  for (const i in reports) {
+    const levels = reports[i].split(' ').map(l => parseInt(l))
+    let levelSkipped = false
+
+    let increasing = false
+
+    const [current, next, after] = levels
+    if (current < next && next <= current + 3) {
+      // increasing
+      increasing = true
+    } else if (current > next && next >= current - 3) {
+      // decreasing
+    } else if (current < after && after <= current + 3) {
+      // increasing
+      increasing = true
+    } else if (current > after && after >= current - 3) {
+      // decreasing
+    }
+
+    if (increasing) {
+      for (let j = 0; j < levels.length; j++) {
+        const currentLevel = levels[j]
+        const nextLevel = levels[j + 1]
+
+        if (!nextLevel) break;
+
+        if (currentLevel === nextLevel) {
+          if (!levelSkipped) {
+            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
+            // safe
+            levelSkipped = true
+            levels.splice(j + 1, 1)
+
+            j--
+            continue;
+          }
+
+          // unsafe
+          reports[i] = false
+          break;
+        } else if (currentLevel < nextLevel && nextLevel <= currentLevel + 3) {
+            console.log('🟢', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
+          // safe
+        } else {
+          if (!levelSkipped) {
+            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
+            // safe
+            levelSkipped = true
+            levels.splice(j + 1, 1)
+
+            j--
+            continue;
+          }
+
+          // unsafe
+          reports[i] = false
+          break;
+        }
+      }
+    } else {
+      for (let j = 0; j < levels.length; j++) {
+        const currentLevel = levels[j]
+        const nextLevel = levels[j + 1]
+
+
+        if (!nextLevel) break;
+
+        if (currentLevel === nextLevel) {
+          if (!levelSkipped) {
+            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
+            // safe
+            levelSkipped = true
+            levels.splice(j + 1, 1)
+            j--
+            continue;
+          }
+
+          // unsafe
+          reports[i] = false
+          break;
+        } else if (currentLevel > nextLevel && nextLevel >= currentLevel - 3) {
+            console.log('🟢', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
+          // safe
+        } else {
+          if (!levelSkipped) {
+            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
+            // safe
+            levelSkipped = true
+            levels.splice(j + 1, 1)
+            j--
+            continue;
+          }
+
+          // unsafe
+          reports[i] = false
+          break;
+        }
+      }
+    }
+  }
+
+  //console.log(performance.now() - start)
+
+  return reports
+}
+
+
--- day-02.test.js
+++ day-02.test.js
@@ -1,13 +1,13 @@
 import test from "node:test"
 import assert from "node:assert/strict"
-import { p1a } from './day-02.js'
+import { p1a, p2a } from './day-02.js'
 import { readFileSync } from 'node:fs'
 
 test('day two', async t => {
   let input = `7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9`
   const inputFile = readFileSync('02-input.txt', 'utf8');
 
-  await t.test('part 1', async t => {
+  await t.skip('part 1', async t => {
     await t.test('p1a sample data', () => {
       const reports = p1a(input)
 
@@ -21,8 +21,8 @@ test('day two', async t => {
     })
 
     await t.test('p1a sample data extended', () => {
-      input = input + '\n35 37 38 41 43 41'
-      const reports = p1a(input)
+      const localInput = input + '\n35 37 38 41 43 41'
+      const reports = p1a(localInput)
 
       assert.equal(reports[0], '7 6 4 2 1')
       assert.equal(reports[1], false)
@@ -37,6 +37,61 @@ test('day two', async t => {
     await t.test('p1a actual data', () => {
       const reports = p1a(inputFile)
       assert.equal(reports.filter(Boolean).length, 524)
+    })
+  })
+
+  await t.test('part 2', async t => {
+    await t.skip('p2a sample data', () => {
+      const reports = p2a(input)
+
+      assert.equal(reports[0], '7 6 4 2 1')
+      assert.equal(reports[1], false)
+      assert.equal(reports[2], false)
+      assert.equal(reports[3], '1 3 2 4 5')
+      assert.equal(reports[4], '8 6 4 4 1')
+      assert.equal(reports[5], '1 3 6 7 9')
+
+      assert.equal(reports.filter(Boolean).length, 4)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = input + '\n35 37 38 41 43 41'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], '7 6 4 2 1')
+      assert.equal(reports[1], false)
+      assert.equal(reports[3], '1 3 2 4 5')
+      assert.equal(reports[4], '8 6 4 4 1')
+      assert.equal(reports[5], '1 3 6 7 9')
+      assert.equal(reports[6], '35 37 38 41 43 41')
+      assert.equal(reports.filter(Boolean).length, 5)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '1 3 2 4 5'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], '1 3 2 4 5')
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '86 86 87 90 90 91 93 93'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], false)
+    })
+
+    await t.test('p2a sample data extended', () => {
+      const input = '8 9 10 11\n48 46 47 49 51 54 56\n1 1 2 3 4 5\n1 2 3 4 5 5\n5 1 2 3 4 5\n1 4 3 2 1\n1 6 7 8 9\n1 2 3 4 3\n9 8 7 6 7\n7 10 8 10 11\n29 28 27 25 26 25 22 20'
+      const reports = p2a(input)
+
+      console.log(reports)
+    })
+
+    await t.skip('p2a actual data', () => {
+      const reports = p2a(inputFile)
+      console.log(reports.filter(Boolean))
+      assert.equal(reports.filter(Boolean).length, 573)
     })
   })
 })
