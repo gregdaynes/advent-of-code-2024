7c693d222d339b3f17bb65f12a6aaaf0e6d891c6
Gregory Daynes <email@gregdaynes.com>
2024-12-02 12:17:59 -0800

    chore(02): pt 2 dead end passing many tests but not all

--- day-02.js
+++ day-02.js
@@ -44,111 +44,179 @@ export function p1a (input) {
 }
 
 export function p2a (input) {
-  const start = performance.now()
   const reports = input.split('\n')
 
-  for (const i in reports) {
-    const levels = reports[i].split(' ').map(l => parseInt(l))
-    let levelSkipped = false
-
-    let increasing = false
-
-    const [current, next, after] = levels
-    if (current < next && next <= current + 3) {
-      // increasing
-      increasing = true
-    } else if (current > next && next >= current - 3) {
-      // decreasing
-    } else if (current < after && after <= current + 3) {
-      // increasing
-      increasing = true
-    } else if (current > after && after >= current - 3) {
-      // decreasing
-    }
-
-    if (increasing) {
-      for (let j = 0; j < levels.length; j++) {
-        const currentLevel = levels[j]
-        const nextLevel = levels[j + 1]
-
-        if (!nextLevel) break;
-
-        if (currentLevel === nextLevel) {
-          if (!levelSkipped) {
-            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
-            // safe
-            levelSkipped = true
-            levels.splice(j + 1, 1)
-
-            j--
-            continue;
-          }
-
-          // unsafe
-          reports[i] = false
-          break;
-        } else if (currentLevel < nextLevel && nextLevel <= currentLevel + 3) {
-            console.log('🟢', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
-          // safe
-        } else {
-          if (!levelSkipped) {
-            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '+')
-            // safe
-            levelSkipped = true
-            levels.splice(j + 1, 1)
-
-            j--
-            continue;
-          }
-
-          // unsafe
-          reports[i] = false
-          break;
-        }
-      }
-    } else {
-      for (let j = 0; j < levels.length; j++) {
-        const currentLevel = levels[j]
-        const nextLevel = levels[j + 1]
-
-
-        if (!nextLevel) break;
-
-        if (currentLevel === nextLevel) {
-          if (!levelSkipped) {
-            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
-            // safe
-            levelSkipped = true
-            levels.splice(j + 1, 1)
-            j--
-            continue;
-          }
-
-          // unsafe
-          reports[i] = false
-          break;
-        } else if (currentLevel > nextLevel && nextLevel >= currentLevel - 3) {
-            console.log('🟢', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
-          // safe
-        } else {
-          if (!levelSkipped) {
-            console.log('🔴', reports[i], { levelSkipped, currentLevel, nextLevel }, '-')
-            // safe
-            levelSkipped = true
-            levels.splice(j + 1, 1)
-            j--
-            continue;
-          }
-
-          // unsafe
-          reports[i] = false
-          break;
-        }
-      }
-    }
+  function isValid (a, b) {
+    if (a < b && b <= a + 3) return true // valid ascend
+    if (a > b && b >= a - 3) return true // valid decend
+    return false
   }
 
-  //console.log(performance.now() - start)
+  for (const report in reports) {
+    console.log('======================')
+    console.log(`report ${report}:`, reports[report])
+    const levels = reports[report].split(' ').map(Number)
+
+    let skipped
+    let safe = true
+
+    // intentionally skip first and last
+    for (let i = 1; i < levels.length - 1; i++) {
+      console.log({ p: levels[i - 1], c: levels[i], n: levels[i+1] })
+
+      const p = i - 1
+      const c = i
+      const n = i + 1
+
+      if (skipped) {
+        if (skipped === p) {
+          console.log('p previously skipped')
+        }
+
+        if (skipped === c) {
+          console.log('c previously skipped')
+
+        }
+
+        if (skipped === n) {
+          console.log('n previously skipped')
+        }
+      }
+
+      const previousCurrent = isValid(levels[i - 1], levels[i])
+      const currentNext = isValid(levels[i], levels[i+1])
+      const previousNext = isValid(levels[i - 1], levels[i+1])
+
+      if (previousCurrent && currentNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+        console.log('🟢')
+
+        continue;
+      }
+
+      if (!previousCurrent && !currentNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i
+          console.log('ccc', 'skipping', i)
+          continue;
+        }
+
+        safe = false
+        break;
+      }
+
+      if (!previousCurrent && !currentNext && previousNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i
+          console.log('aa', 'skipping', i)
+          continue;
+        }
+
+        safe = false
+        break;
+      }
+
+      if (!previousCurrent && currentNext && previousNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i - 1
+          console.log('bb', 'skipping', i - 1)
+          continue;
+        }
+
+
+        safe = false
+        break;
+      }
+
+      if (previousCurrent && !currentNext) {
+         console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i + 1
+          console.log('xx', 'skipping', i + 1)
+          continue;
+        }
+
+        safe = false
+        break;
+      }
+
+
+
+      if (!previousCurrent && !previousNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i - 1
+          console.log('yy', 'skipping', i - 1)
+          continue;
+        }
+
+
+        safe = false
+        break;
+      }
+
+      if (!previousCurrent && !currentNext) {
+        console.log({ previousCurrent, currentNext, previousNext, skipped })
+
+        if (skipped === undefined) {
+          skipped = i - 1
+          console.log('zz', 'skipping', i - 1)
+          continue;
+        }
+
+        safe = false
+        break;
+      }
+
+      console.log('breakout', { previousCurrent, currentNext, previousNext, skipped })
+    }
+
+    if (safe === false) {
+      console.log('unsafe')
+      reports[report] = false
+      continue;
+    }
+
+
+    let reportWithoutSkipped = reports[report].split(' ').map(Number)
+    reportWithoutSkipped[skipped] = undefined
+    reportWithoutSkipped = reportWithoutSkipped.filter(Boolean)
+    console.log(reports[report], reportWithoutSkipped.filter(Boolean))
+
+    let direction
+
+    for (let i = 0; i < reportWithoutSkipped.length; i++) {
+      const c = i;
+      const n = i + 1;
+
+      if (!n) {
+        continue;
+      }
+
+      if (!direction) {
+        direction = c < n ? 1 : -1
+      }
+
+      if (direction === 1 && c < n) continue
+      if (direction === -1 && c > n) continue
+
+      safe = false
+      break;
+    }
+
+    console.log('xxxxxxxxxxxxxxxxxxxx')
+    console.log('direction', direction, safe)
+
+    reports[report] = safe
+  }
 
   return reports
 }
--- day-02.test.js
+++ day-02.test.js
@@ -41,37 +41,34 @@ test('day two', async t => {
   })
 
   await t.test('part 2', async t => {
-    await t.skip('p2a sample data', () => {
+    await t.test('p2a sample data', () => {
       const reports = p2a(input)
 
-      assert.equal(reports[0], '7 6 4 2 1')
+      assert.equal(reports[0], true)
       assert.equal(reports[1], false)
       assert.equal(reports[2], false)
-      assert.equal(reports[3], '1 3 2 4 5')
-      assert.equal(reports[4], '8 6 4 4 1')
-      assert.equal(reports[5], '1 3 6 7 9')
-
-      assert.equal(reports.filter(Boolean).length, 4)
+      assert.equal(reports[3], true)
+      assert.equal(reports[4], true)
+      assert.equal(reports[5], true)
     })
 
     await t.skip('p2a sample data extended', () => {
-      const input = input + '\n35 37 38 41 43 41'
-      const reports = p2a(input)
+      const reports = p2a(input + '\n35 37 38 41 43 41')
 
-      assert.equal(reports[0], '7 6 4 2 1')
+      assert.equal(reports[0], true)
       assert.equal(reports[1], false)
-      assert.equal(reports[3], '1 3 2 4 5')
-      assert.equal(reports[4], '8 6 4 4 1')
-      assert.equal(reports[5], '1 3 6 7 9')
-      assert.equal(reports[6], '35 37 38 41 43 41')
-      assert.equal(reports.filter(Boolean).length, 5)
+      assert.equal(reports[2], false)
+      assert.equal(reports[3], true)
+      assert.equal(reports[4], true)
+      assert.equal(reports[5], true)
+      assert.equal(reports[6], true)
     })
 
     await t.skip('p2a sample data extended', () => {
       const input = '1 3 2 4 5'
       const reports = p2a(input)
 
-      assert.equal(reports[0], '1 3 2 4 5')
+      assert.equal(reports[0], true)
     })
 
     await t.skip('p2a sample data extended', () => {
@@ -81,11 +78,81 @@ test('day two', async t => {
       assert.equal(reports[0], false)
     })
 
-    await t.test('p2a sample data extended', () => {
+    await t.skip('p2a sample data extended', () => {
       const input = '8 9 10 11\n48 46 47 49 51 54 56\n1 1 2 3 4 5\n1 2 3 4 5 5\n5 1 2 3 4 5\n1 4 3 2 1\n1 6 7 8 9\n1 2 3 4 3\n9 8 7 6 7\n7 10 8 10 11\n29 28 27 25 26 25 22 20'
       const reports = p2a(input)
 
-      console.log(reports)
+      assert.equal(reports.filter(Boolean).length, 11)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '9 7 6 2 1'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], false)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '9 7 5 2 1'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '2 6 1'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '2 1 3 5 8'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '13 9 11 9 6'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '48 44 45 44 41 39 36 34'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '1 5 6'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.skip('p2a sample data extended', () => {
+      const input = '52 52 51 52 52'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], false)
+    })
+
+    await t.test('p2a sample data extended', () => {
+      const input = '8 6 4 4 1'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], true)
+    })
+
+    await t.test('p2a sample data extended', () => {
+      const input = '1 2 7 8 9'
+      const reports = p2a(input)
+
+      assert.equal(reports[0], false)
     })
 
     await t.skip('p2a actual data', () => {
