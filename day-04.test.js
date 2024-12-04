a8a9435b0c03c0fbbb42003b56f50817715dbdfe
Gregory Daynes <email@gregdaynes.com>
2024-12-03 21:36:49 -0800

    chore(04:) My plan
    Comments in code outlining the plan

--- day-04.test.js
+++ day-04.test.js
@@ -1,6 +1,7 @@
 import test from "node:test"
 import assert from "node:assert/strict"
 import { readFileSync } from 'node:fs'
+import { p1a } from './day-04.js'
 
 test('day four', async t => {
   let input =
@@ -20,6 +21,19 @@ test('day four', async t => {
     // find all the occurrances of XMAS
     // horizontal vertical, diagonal, backwards, overlaping
 
+    // if we turn the input into a 2d array, we can use [x,y] coordinates to find characters
+    // a function to calculate each direction
+    // when converting to 2d array, index all X positions
+    // for each X
+    // - take [x,y]
+    // - compute the horizontal coordinates for each letter M A S - [x+1,y][x+2,y][x+3,y]
+    // - repeat for direction - should be 8 in total
+    // - if direction can't be computed - eg: too close to an edge, skip it.
+    // - test each direction letter coords for a match
+    // - sum complete matches per X
+    // sum all matches
 
+    const results = p1a(input)
+    console.log(results)
   })
 })
