3e1b7226f4f57e7218dafadca8b96ff9d4e27072
Gregory Daynes <email@gregdaynes.com>
2024-12-03 22:25:42 -0800

    chore(04): working implementation
    It is by no means quick, however easy enough to brute force looking at
    each X and generating a list of coordinates to check.
    
    For some reason, the guard clauses on the directions generator were
    causing a miscount of 17 instead of 18 on the sample data. This should
    be investigated.
    
    I can't really think of ways to improve this, but I'm pretty sure there
    is an way to more efficiently map the directions to check, perhaps
    returning the value at the same time - why store the coord  multple
    times when we can do the check in the same pass.

---
+++ day-04.js
@@ -0,0 +1,112 @@
+function verticalBackwards(max, [y,x]) {
+  //if (y - 3 >= 0) {
+    return [[y,x], [y-1,x], [y-2,x], [y-3,x]]
+  //}
+}
+
+function upright([yMax, xMax], [y,x]) {
+  //if (y - 3 >= 0 && x + 3 <= xMax) {
+    return [[y,x], [y-1,x+1], [y-2,x+2], [y-3,x+3]]
+  //}
+}
+
+function horizontal([yMax, xMax], [y,x]) {
+  //if (x + 3 <= xMax) {
+    return [[y,x], [y,x+1], [y,x+2], [y,x+3]]
+  //}
+}
+
+function downright([yMax, xMax], [y,x]) {
+  //if (y + 3 <= yMax && x + 3 <= xMax) {
+    return [[y,x], [y+1,x+1], [y+2,x+2], [y+3,x+3]]
+  //}
+}
+
+function vertical([yMax, xMax], [y,x]) {
+  //if (y + x <= yMax) {
+    return [[y,x], [y+1,x], [y+2,x], [y+3,x]]
+  //}
+}
+
+function downleft([yMax, xMax], [y,x]) {
+  //if (y + 3 <= yMax && x - 3 >= 0) {
+    return [[y,x], [y+1,x-1], [y+2,x-2], [y+3,x-3]]
+  //}
+}
+
+function horizontalBackwards([yMax, xMax], [y,x]) {
+  //if (x - 3 >= 0) {
+    return [[y,x], [y,x-1], [y,x-2], [y,x-3]]
+  //}
+}
+
+function upleft(max, [y,x]) {
+  //if (y - 3 >= 0 && x - 3 >= 0) {
+    return [[y,x], [y-1,x-1], [y-2,x-2], [y-3,x-3]]
+  //}
+}
+
+export function p1a (input) {
+  // if we turn the input into a 2d array, we can use [x,y] coordinates to find characters
+  // a function to calculate each direction
+  // when converting to 2d array, index all X positions
+  // for each X
+  // - take [x,y]
+  // - compute the horizontal coordinates for each letter M A S - [x+1,y][x+2,y][x+3,y]
+  // - repeat for direction - should be 8 in total
+  // - if direction can't be computed - eg: too close to an edge, skip it.
+  // - test each direction letter coords for a match
+  // - sum complete matches per X
+  // sum all matches
+
+  // convert input into 2d array.
+  const input2dArray = input.split('\n').map(row => row.split(''))
+  const xMax = input2dArray[0].length
+  const yMax = input2dArray.length
+  const max = [yMax, xMax]
+
+  // find all X with coords [y,x]
+  const xMap = []
+  for (const y in input2dArray) {
+    for (const x in input2dArray[y]) {
+      if (input2dArray[y][x] === 'X') {
+        xMap.push([Number(y), Number(x)])
+      }
+    }
+  }
+
+  const xValidDirections = []
+  for (const coord of xMap) {
+    const allCoords = [
+      verticalBackwards,
+      upright,
+      horizontal,
+      downright,
+      vertical,
+      downleft,
+      horizontalBackwards,
+      upleft
+    ].map(fn => {
+      return fn(max, coord)
+    })
+
+    xValidDirections.push(allCoords)
+  }
+
+  let count = 0
+  for (const validDirections of xValidDirections) {
+    for (const direction of validDirections) {
+      if (!direction) continue;
+
+      const word = direction.map(([y, x]) => input2dArray[y]?.[x]).join('')
+
+      if (word === 'XMAS') {
+        //console.log(direction, word)
+        count += 1
+      }
+    }
+  }
+
+  return count
+}
+
--- day-04.test.js
+++ day-04.test.js
@@ -17,23 +17,30 @@ test('day four', async t => {
     'MXMXAXMASX'.trim()
   const inputFile = readFileSync('04-input.txt', 'utf8').trim();
 
-  await t.test('part 1', () => {
-    // find all the occurrances of XMAS
-    // horizontal vertical, diagonal, backwards, overlaping
+  await t.test('part 1', async t => {
+    await t.test('part 1 sample data', () => {
+      // find all the occurrances of XMAS
+      // horizontal vertical, diagonal, backwards, overlaping
 
-    // if we turn the input into a 2d array, we can use [x,y] coordinates to find characters
-    // a function to calculate each direction
-    // when converting to 2d array, index all X positions
-    // for each X
-    // - take [x,y]
-    // - compute the horizontal coordinates for each letter M A S - [x+1,y][x+2,y][x+3,y]
-    // - repeat for direction - should be 8 in total
-    // - if direction can't be computed - eg: too close to an edge, skip it.
-    // - test each direction letter coords for a match
-    // - sum complete matches per X
-    // sum all matches
+      // if we turn the input into a 2d array, we can use [x,y] coordinates to find characters
+      // a function to calculate each direction
+      // when converting to 2d array, index all X positions
+      // for each X
+      // - take [x,y]
+      // - compute the horizontal coordinates for each letter M A S - [x+1,y][x+2,y][x+3,y]
+      // - repeat for direction - should be 8 in total
+      // - if direction can't be computed - eg: too close to an edge, skip it.
+      // - test each direction letter coords for a match
+      // - sum complete matches per X
+      // sum all matches
 
-    const results = p1a(input)
-    console.log(results)
+      const results = p1a(input)
+      assert.equal(results, 18)
+    })
+
+    await t.test('part 1 actual data', () => {
+      const results = p1a(inputFile)
+      assert.equal(results, 2583)
+    })
   })
 })
