9da939af3592f23da6f2021473f146204522b4f3
Gregory Daynes <email@gregdaynes.com>
2024-12-05 09:57:04 -0800

    chore(05): pt2 working implementation
    Moving elements in an array is tedious. I don't like splice. Hopefully
    we'll get a Array.prototype.move in the future. Preferrably a
    not-mutable version (yes we can add our own, but I don't like modifying
    prototypes).
    
    This approach is alright, I wonder if there is something better. Like,
    do we have to sort all the numbers? or do we just have to ensure the
    middle number is valid?

--- day-05.js
+++ day-05.js
@@ -36,47 +36,98 @@ export function p1a (input) {
   // evaluation
 
   const validUpdates = []
+  const invalidUpdates = []
 
   for (const update of updates) {
-    let isValid = true
-
-    update:
-    for (const pageIndex in update) {
-      if (!isValid) break update
-
-      const pageNumber = update[pageIndex]
-      const applicableRules = rules[pageNumber]
-
-      if (!applicableRules || !applicableRules.length) continue update;
-
-      follower:
-      for (const follower of applicableRules) {
-        const followerIndex = update.indexOf(follower)
-
-        // follower doesn't exist in update, ignore
-        if (followerIndex === -1) continue follower
-
-        // invalid update, exit
-        if (followerIndex < pageIndex) {
-          console.log({
-            update: '❌',
-            pageNumber,
-            pageIndex,
-            applicableRules,
-            followerIndex,
-            follower,
-          })
-          isValid = false
-          break update
-        }
-      }
+    const result = validate(update, rules)
+    if (result !== true) {
+      invalidUpdates.push(update)
+      continue;
     }
 
-    if (isValid) validUpdates.push(update)
+    validUpdates.push(update)
   }
 
-  return validUpdates.reduce((acc, update) => {
+  const validUpdateMiddlePageScore = validUpdates.reduce((acc, update) => {
     const mid = Math.floor(update.length / 2)
     return acc += update[mid]
   }, 0)
+
+  // -- part 2
+
+  // Now we have to sort the invalid updates
+  // We can extract the validator function to use on both steps
+  // with a validator in place, when a rule fails, we can return the index of the number that the page should be before
+  // and then update the update ordering by moving the page to returned index - 1
+  // then rerun the validator, and repeat as necessary
+
+  const correctedUpdates = []
+  for (const update of invalidUpdates) {
+    let isValid = false;
+    let updateClone = [...update]
+
+    let i = 0
+    while (!isValid) {
+      const result = validate(updateClone, rules)
+
+      if (result !== true) {
+        moveArrayItem(updateClone, result.pageIndex, result.followerIndex)
+        continue;
+      }
+
+      isValid = true
+    }
+
+    correctedUpdates.push(updateClone)
+  }
+
+  const correctedUpdateMiddlePageScore = correctedUpdates.reduce((acc, update) => {
+    const mid = Math.floor(update.length / 2)
+    return acc += update[mid]
+  }, 0)
+
+  return [validUpdateMiddlePageScore, correctedUpdateMiddlePageScore]
+}
+
+function moveArrayItem (array, from,to) {
+  array.splice(to, 0, array.splice(from, 1)[0])
+}
+
+function validate (update, rules) {
+  let result = true;
+
+  update:
+  for (let pageIndex in update) {
+    if (result !== true) break update
+    pageIndex = Number(pageIndex)
+
+    const pageNumber = update[pageIndex]
+    const applicableRules = rules[pageNumber]
+
+    if (!applicableRules || !applicableRules.length) continue update;
+
+    follower:
+    for (const follower of applicableRules) {
+      const followerIndex = update.indexOf(follower)
+
+      // follower doesn't exist in update, ignore
+      if (followerIndex === -1) continue follower
+
+      // invalid update, exit
+      if (followerIndex < pageIndex) {
+        //console.log({
+        //  update: '❌',
+        //  pageNumber,
+        //  pageIndex,
+        //  applicableRules,
+        //  followerIndex,
+        //  follower,
+        //})
+        result = { pageNumber, pageIndex, followerIndex }
+        break update
+      }
+    }
+  }
+
+  return result
 }
--- day-05.test.js
+++ day-05.test.js
@@ -10,12 +10,24 @@ test('day four', async t => {
   await t.test('part 1', async t => {
     await t.test('part 1 sample data', () => {
       const results = p1a(input)
-      assert.equal(results, 143)
+      assert.equal(results[0], 143)
     })
 
     await t.test('part 1 actual data', () => {
       const results = p1a(inputFile)
-      assert.equal(results, 4996)
+      assert.equal(results[0], 4996)
+    })
+  })
+
+  await t.test('part 2', async t => {
+    await t.test('part 2 sample data', () => {
+      const results = p1a(input)
+      assert.equal(results[1], 123)
+    })
+
+    await t.test('part 2 actual data', () => {
+      const results = p1a(inputFile)
+      assert.equal(results[1], 6311)
     })
   })
 })
