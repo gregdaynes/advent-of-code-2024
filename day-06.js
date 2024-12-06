ae4933248a7b9fb16b5fbb1af93c8343cf12cc70
Gregory Daynes <email@gregdaynes.com>
2024-12-06 08:32:40 -0800

    chore(06): pt1 plan

--- day-06.js
+++ day-06.js
@@ -1,3 +1,20 @@
 export function p1a (input) {
+  // actor is represented by ^
+  // obstructions represented by #
+  // when actor's next move would be into an obstruction turn to right
+  // repeat until actor leaves area
+  // return number of moves actor has made
+
+  // 0) parse input into 2d array - maybe shortcut and record position of all obstructions
+  // 1) get the actor position
+  // 2) get the direction vector of the actor
+  // 3) get position of object in vector path or bounds
+  // 3b) if bounds, add all spaces to total and return
+  // 4) record number of DISTINCT positions
+  // 5) rotate direction vector
+  // 6) repeat
+
+
+
   return 41
 }
