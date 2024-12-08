cca739c4fef386597538eb7d8b82e19c9abb5ce0
Gregory Daynes <email@gregdaynes.com>
2024-12-07 17:57:52 -0800

    chore(07): pt2 working implementation
    Using the array of operations works really well for extending with new
    operations. I think if the original approach would have exploded
    horribly. Yay.
    
    I think we can reduce the duplication between the methods, as long as we
    can instruct the solver function to add the third operation to the
    array. But otherwise this is done.

--- day-07.js
+++ day-07.js
@@ -5,17 +5,18 @@ export function p1a (input) {
 
   let sum = 0
   for (const row of rows) {
-    const x = perform(...row)
+    const test = row[0]
+    const result = perform1(...row)
 
-    if (x === row[0]) {
-      sum += row[0]
+    if (result === test) {
+      sum += test
     }
   }
 
   return sum
 }
 
-function perform (target, ...numbers) {
+function perform1 (target, ...numbers) {
   const operations = [add, multiply]
 
   // early exit if operation exceeds target
@@ -29,10 +30,11 @@ function perform (target, ...numbers) {
   }
 
   for (const operation of operations) {
-    const result = operation(numbers[0], numbers[1])
-    const recurse = perform(target, result, ...numbers.slice(2))
+    const rest = numbers.slice(2)
+    const operationResult = operation(numbers[0], numbers[1])
+    const result = perform1(target, operationResult, ...rest)
 
-    if (recurse) {
+    if (result) {
       return target
     }
   }
@@ -40,10 +42,62 @@ function perform (target, ...numbers) {
   return false
 }
 
+// --------------------------------------------------
+
+export function p2a (input) {
+  const rows = input.split('\n')
+    .map(row => row.split(/[^\d]+/))
+    .map(row => row.map(Number))
+
+  let sum = 0
+  for (const row of rows) {
+    const test = row[0]
+    const result = perform2(...row)
+
+    if (result === test) {
+      sum += test
+    }
+  }
+
+  return sum
+}
+
+function perform2 (target, ...numbers) {
+  const operations = [add, multiply, combine]
+
+  // early exit if operation exceeds target
+  if (numbers[0] > target) {
+    return false
+  }
+
+  // if only a single number, check
+  if (numbers.length === 1) {
+    return numbers[0] === target
+  }
+
+  for (const operation of operations) {
+    const rest = numbers.slice(2)
+    const operationResult = operation(numbers[0], numbers[1])
+    const result = perform2(target, operationResult, ...rest)
+
+    if (result) {
+      return target
+    }
+  }
+
+  return false
+}
+
+// -------------------------------------------------
+
 function add(a, b) {
   return a + b
 }
 
 function multiply(a, b) {
   return a * b
+}
+
+function combine(a, b) {
+  return Number(a + '' + b)
 }
--- day-07.test.js
+++ day-07.test.js
@@ -1,7 +1,7 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { readFileSync } from "node:fs";
-import { p1a } from "./day-07.js";
+import { p1a, p2a } from "./day-07.js";
 
 test("day seven", async (t) => {
   const input = readFileSync("07-sample-input.txt", "utf8").trim();
@@ -16,6 +16,18 @@ test("day seven", async (t) => {
     await t.test("part 1 actual data", () => {
       const results = p1a(inputFile);
       assert.equal(results, 2314935962622);
+    });
+  });
+
+  await t.test("part 2", async (t) => {
+    await t.test("part 2 sample data", () => {
+      const results = p2a(input);
+      assert.equal(results, 11387);
+    });
+
+    await t.test("part 2 actual data", () => {
+      const results = p2a(inputFile);
+      assert.equal(results, 401477450831495);
     });
   });
 });
