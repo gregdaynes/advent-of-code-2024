ee1959029b76ec5e7aa9b079043f729a45bd6389
Gregory Daynes <email@gregdaynes.com>
2024-12-07 18:38:00 -0800

    chore(07): firest attempt to avoid passing operations
    I don't care for this. It's nice, but makes the whole thing harder to
    look at. But I do like not having to pass around options.

--- day-07.js
+++ day-07.js
@@ -6,38 +6,35 @@ export function p1a (input, part2 = false) {
   const operations = [add, multiply]
   if (part2) operations.push(combine)
 
-  return rows.reduce((acc, numbers) =>
-    acc += perform(operations, ...numbers), 0)
-}
+  function perform (target, ...numbers) {
+    // early exit if operation exceeds target
+    if (numbers[0] > target) return 0
 
-function perform (operations, target, ...numbers) {
-  // early exit if operation exceeds target
-  if (numbers[0] > target) {
+    // if only a single number, check
+    if (numbers.length === 1) {
+      return numbers[0] === target ? numbers[0] : 0
+    }
+
+    for (const operation of operations) {
+      const operationResult = operation(...numbers)
+      const rest = numbers.slice(2)
+
+      const result = perform(
+        target,
+        operationResult,
+        ...rest
+      )
+
+      if (result) {
+        return result
+      }
+    }
+
     return 0
   }
 
-  // if only a single number, check
-  if (numbers.length === 1) {
-    return numbers[0] === target ? numbers[0] : 0
-  }
-
-  for (const operation of operations) {
-    const operationResult = operation(...numbers)
-    const rest = numbers.slice(2)
-
-    const result = perform(
-      operations,
-      target,
-      operationResult,
-      ...rest
-    )
-
-    if (result) {
-      return result
-    }
-  }
-
-  return 0
+  return rows.reduce((acc, numbers) =>
+    acc += perform(...numbers), 0)
 }
 
 function add(a, b) {
